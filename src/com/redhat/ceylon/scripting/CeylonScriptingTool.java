0b45ad260d33d6c3282ca2349eb570a53dffed6a
Miguel Angel Gordian <os.aioria@gmail.com>
2015-07-03 04:14:41 -0500

    entrypoint

--- src/com/redhat/ceylon/scripting/CeylonScriptingTool.java
+++ src/com/redhat/ceylon/scripting/CeylonScriptingTool.java
@@ -5,18 +5,22 @@ import java.io.File;
 import com.redhat.ceylon.scripting.compile.Generator;
 import com.redhat.ceylon.scripting.run.Executer;
 
-class CeylonScriptingTool {
+public class CeylonScriptingTool {
 	public static void main(String[] args) {
 		System.out.println("Executing script");
+		new CeylonScriptingTool().run(args);
+	}
+	
+	public Integer run(String[] args) {
 		try {
-			
 			Generator producer = new Generator();
 			Executer run = new Executer();
-			
+		
 			File code = producer.intermediteCode(args);
 			run.run(code, args);
+			return 0;
 		} catch (Exception e) {
-			
+			return -1;
 		}
 	}
 }
